43c884c357da4c37bdb49367b8a882eab4af772e
luti <lucien123456@163.com>
2024-01-09 00:53:52 +0800

    modified:   app/__pycache__/form.cpython-39.pyc 	modified:   app/__pycache__/router.cpython-39.pyc 	modified:   app/form.py 	modified:   app/router.py 	new file:   app/static/css/自定义样式/my_login.css 	modified:   app/templates/index.html 	modified:   app/templates/login.html 	modified:   app/templates/regist.html 	new file:   app/templates/test.html

--- app/router.py
+++ app/router.py
@@ -1,55 +1,52 @@
-from flask import Flask,redirect,render_template,request,url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask,redirect,render_template,request,url_for,flash
 from flask_bootstrap import Bootstrap
 from .config import Config
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 app.config.from_object(Config)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:12345678@localhost:3307/选课系统?charset=utf8mb4'
 
-from .form import RegistForm
+from .form import RegistForm,loginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
 
-@app.route('/login',methods=['GET','POST'])
-def login():
-    username = {'username':'Miguel'}
-    password = {'password':'12345678'}
-    if request.method == 'POST':
-        name = request.form.get('name')
-        password = request.form.get('password')
-        if username == 'Miguel' and password == '12345678':
-            return render_template('index.html')
-        else:
-            return '登录失败'
-    else:
-        return render_template('login.html')
+@app.route('/test')
+def test():
+    return render_template('test.html',form = loginForm())
 
 
+#用户注册
 @app.route('/regist',methods=['GET','POST'])
 def regist():
     if request.method == 'GET':
-        return render_template('regist.html')
+        RetForm = RegistForm()
+        return render_template('regist.html', form=RetForm)
     else:
-        name = request.form.get('name')
-        password = request.form.get('password')
-        regist_form = RegistForm()
-        if regist_form.validate_on_submit():
-            return redirect(url_for('login'))
+        RetForm = RegistForm()
+        if RetForm.validate():  #validate函数前端实时验证用户输入格式是否正确
+            print('接收到的参数为：', RetForm.data)
+            return '''<script>alert('您的注册请求已提交!');</script>'''
         else:
-            return '参数不全'
+             print(RetForm.errors)
+        return render_template('login.html', form=RetForm)
 
-
-        # if name and password:
-        #     user = User(name=name,password=password)
-        #     db.session.add(user)
-        #     db.session.commit()
-        #     return redirect(url_for('login'))
-        # else:
-        #     return '参数不全'
+#用户登录
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        RetForm = loginForm()
+        return render_template('login.html', form=RetForm)
+    else:
+        RetForm = loginForm(formdata=request.form)
+        if RetForm.validate():
+            temp = RetForm.data
+            print('接收到的参数为：', temp)
+            return '''<script>alert('您的登录请求已提交!');</script>'''
+        return render_template('index.html', form=RetForm)
 
 
 
