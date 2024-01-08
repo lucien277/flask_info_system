9134823a062ae4a006823b5896bfc8f0fd6f1e9d
luti <lucien123456@163.com>
2024-01-09 00:53:52 +0800

    modified:   app/__pycache__/form.cpython-39.pyc 	modified:   app/__pycache__/router.cpython-39.pyc 	modified:   app/form.py 	modified:   app/router.py 	new file:   app/static/css/自定义样式/my_login.css 	modified:   app/templates/index.html 	modified:   app/templates/login.html 	modified:   app/templates/regist.html 	new file:   app/templates/test.html

--- app/form.py
+++ app/form.py
@@ -1,14 +1,50 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, StringField, PasswordField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms import form,SubmitField, StringField, PasswordField, BooleanField,widgets
+from wtforms.validators import DataRequired,ValidationError,EqualTo,Regexp,Length
+from wtforms.fields import simple
 
 class loginForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
+    username = StringField('username', validators=[DataRequired()])
     password = StringField('password',validators=[DataRequired()])
     remember_me = BooleanField('remember_me')
     submit = SubmitField('submit')
-class RegistForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
-    password = StringField('password',validators=[DataRequired()])
-    submit = SubmitField('submit')
 
+class RegistForm(FlaskForm):
+    username = simple.StringField(
+        label='用户名:',
+        validators=[
+            DataRequired(message='用户名不能为空'),
+            Length(min=3,max=12,message='用户名长度必须在3-12位之间')
+            ],
+        widget=widgets.TextInput(),
+        render_kw={'class': 'form-control',
+                   "placeholder":"输入注册用户名"}
+        )
+
+    password = simple.PasswordField(
+        label='密码:',
+        validators=[
+            DataRequired(message='密码不能为空'),
+            Length(min=3,max=12,message='密码长度必须在3-12位之间'),
+            Regexp(regex="[0-9a-zA-Z]{5,}",message='密码不允许使用特殊字符')
+            ],
+        widget=widgets.PasswordInput(),
+        render_kw={'class': 'form-control',
+                   "placeholder":"输入注册密码"}
+        )
+
+    repassword = simple.PasswordField(
+        label='确认密码:',
+        validators=[
+            DataRequired(message='密码不能为空'),
+            EqualTo('password',message='两次密码不一致')
+            ],
+        widget=widgets.PasswordInput(),
+        render_kw={'class': 'form-control',
+                   "placeholder":"确认密码"}
+        )
+
+    Submit = SubmitField(
+        label='注册',
+        render_kw={'class': 'btn btn-primary'}
+        )
