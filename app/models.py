511eab9fe31a6a90187dccc40ae302af9681af4f
luti <lucien123456@163.com>
2024-01-10 10:47:14 +0800

    add two models

--- app/models.py
+++ app/models.py
@@ -4,9 +4,8 @@ class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(80),unique=True,nullable=False)
-    email = db.Column(db.String(120),unique=True,nullable=False)
+    email = db.Column(db.String(120),unique=True,nullable=True)
     password = db.Column(db.String(120),nullable=False)
-    email = db.Column(db.String(120),unique=True,nullable=False)
     post = db.relationship('Post',backref='author',lazy='dynamic')
     # comment = db.relationship('Comment',backref='author',lazy='dynamic')
 
--- run_server.py
+++ run_server.py
@@ -1,5 +1,10 @@
-from app.router import app
+from app.router import app,db
+from app.models import User,Post
 
-app.secret_key = 'this is secret key'
 
-app.run(host='127.0.0.1', port=8080, debug=True)
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
